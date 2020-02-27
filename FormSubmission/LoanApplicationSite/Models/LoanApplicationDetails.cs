0d4394c4d02dfbca669446fdc804217ee704d24c
William Farmer <willetfarm@yahoo.com>
2020-02-27 13:01:18 +0000

    Add file via upload

---
+++ FormSubmission/LoanApplicationSite/Models/LoanApplicationDetails.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace LoanApplicationSite.Models
+{
+    public class LoanApplicationDetails
+    {
+        [Required]
+        [DisplayName("First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [DisplayName("Second Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [DisplayName("Existing Account")]
+        public AccountType ExistingAccount { get; set; }
+
+        [Required]
+        [DisplayName("I accept the terms and conditions")]
+        [Range(typeof(bool), "true", "true", ErrorMessage = "You must accept the terms and conditions")]
+        public bool TermsAcceptance { get; set; } 
+    }
+}
