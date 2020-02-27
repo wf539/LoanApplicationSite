c76ac0a551e22cc7021d0021b019fb1daf145073
William Farmer <willetfarm@yahoo.com>
2020-02-27 12:54:25 +0000

    Add file via upload

---
+++ FormSubmission/LoanApplicationSite/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System.Web.Mvc;
+using LoanApplicationSite.Models;
+
+namespace LoanApplicationSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult StartLoanApplication()
+        {            
+            return View(new LoanApplicationDetails {ExistingAccount=AccountType.None});
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult SubmitApplication(LoanApplicationDetails applicationDetails)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("StartLoanApplication", applicationDetails);
+            }
+
+            SimulateSavingToDatabaseOrBackendSystem();
+            
+            return View("ApplicationComplete", new ApplicationCompleteConfirmation {FirstName= applicationDetails.FirstName});
+        }
+
+        private void SimulateSavingToDatabaseOrBackendSystem()
+        {
+            // ...
+        }
+
+        public ActionResult ApplicationComplete(ApplicationCompleteConfirmation confirmationDetails)
+        {
+            return View(confirmationDetails);
+        }
+    }
+}
