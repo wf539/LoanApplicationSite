14080eb6a8a0fb092885cb938a40c6bf0665942a
William Farmer <willetfarm@yahoo.com>
2020-02-27 14:07:24 +0000

    Add file via upload

---
+++ SecondTest/LoanApplicationTests.cs
@@ -0,0 +1,27 @@
+﻿using Xunit;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace DemoWebApp.Tests
+{
+    public class LoanApplicationTests
+    {
+        [Fact]
+        public void StartApplication()
+        {
+            using (IWebDriver driver = new FirefoxDriver())
+            {
+                driver.Manage().Window.Maximize();
+
+                driver.Navigate().GoToUrl("http://localhost:40077/");
+
+                IWebElement applicationButton = driver.FindElement(By.Id("startApplication"));
+
+                applicationButton.Click();
+
+                Assert.Equal("Start Loan Application - Loan Application", driver.Title);
+            }
+        }
+
+    }
+}
