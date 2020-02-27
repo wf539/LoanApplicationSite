42881c4370020b0ab377d2b294f7973e5b0a5180
William Farmer <willetfarm@yahoo.com>
2020-02-27 12:42:16 +0000

    Add file via upload

---
+++ FormSubmission/LoanApplicationTests.cs
@@ -0,0 +1,49 @@
+﻿using Xunit;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace DemoWebApp.Tests
+{
+    public class LoanApplicationTests
+    {
+        [Fact]
+        public void StartApplication()
+        {
+            using (IWebDriver driver = new FirefoxDriver())
+            {
+                driver.Manage().Window.Maximize();
+
+                driver.Navigate().GoToUrl("http://localhost:40077/");
+
+                IWebElement applicationButton = driver.FindElement(By.Id("startApplication"));
+
+                applicationButton.Click();
+
+                Assert.Equal("Start Loan Application - Loan Application", driver.Title);
+            }
+        }
+
+        [Fact]
+        public void SubmitApplication()
+        {
+            using (IWebDriver driver = new FirefoxDriver())
+            {
+                driver.Manage().Window.Maximize();
+
+                driver.Navigate().GoToUrl("http://localhost:40077/Home/StartLoanApplication");
+
+                IWebElement firstNameInput = driver.FindElement(By.Id("FirstName"));
+                firstNameInput.SendKeys("Sarah");
+
+                driver.FindElement(By.Id("LastName")).SendKeys("Smith");
+
+
+                driver.FindElement(By.Id("Loan")).Click();
+
+
+                driver.FindElement(By.Name("TermsAcceptance")).Click();
+            }
+        }
+
+    }
+}
